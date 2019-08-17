75c136c28fd66a50bb13b4b03f5428d285b0f13a
bellakratchei <53630203+bellakratchei@users.noreply.github.com>
2019-08-16 22:21:46 -0300

    Add files via upload

---
+++ exercicio1.php
@@ -0,0 +1,11 @@
+<?php
+
+$tempo=readline ('Digite o tempo gasto na viagem:' . PHP_EOL); 
+$velocidade=readline ('Informe a velocidade média:' . PHP_EOL); 
+$distancia=$tempo*$velocidade;
+$litros=$distancia/12;
+
+echo "A velocidade média durante a viagem foi de: $velocidade" . PHP_EOL;
+echo "O tempo gasto durante a viagem foi de : $tempo" . PHP_EOL;
+echo "A distância percorrida na viagem foi de: $distancia" . PHP_EOL;
+echo "A quantidade de litros utilizada durante a viagem foi: $litros" . PHP_EOL;
